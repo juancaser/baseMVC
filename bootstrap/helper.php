6aaecb8ee172af66b69350a2a5fc11a718943f4b
Jan Caser <Jan Caser>
2016-05-13 10:08:44 +0800

    Commit bootstrap helper

--- bootstrap/helper.php
+++ bootstrap/helper.php
@@ -11,34 +11,71 @@
 /**
  * _getenv
  *
+ * @version 1
+ * @subpackage 1
+ * 
  * @param string $varname
  * @param string $default
  * @return string
  **/
-function _getenv($varname,$default = NULL){
+function _getEnv($varname,$default = NULL){
      if(!$var = getenv($varname)){
           return $default;
      }else{
-          return $var;
+          return (!empty($var) ? $var : $default);
      }
+}
+
+/**
+ * Convert array to array to object
+ * 
+ * @version 1
+ * @subpackage 1
+ *
+ * @param array $array
+ * @return array
+ **/
+function convertArrToObj($array){
+     return json_decode(json_encode($array));
 }
 
 /**
  * Loads config file
  *
+ * @version 1
+ * @subpackage 1
+ *
  * @param string $key
  * @return string/array
  **/
 function config($key,$default = null){
-     $cex = explode('.',$key);
-     $config_file = array_shift($cex);
-     if(file_exist(dirname(__DIR__)."/config/$config_file.php")){
-          $configs = require dirname(__DIR__)."/config/$config_file.php";          
-          iterator_apply($iterator, function(){
-          }, array($iterator));
-     }     
+     
+     $keys = explode('.',$key);
+     $config_file = array_shift($keys); // Config file
+     
+     if (file_exists(dirname(__DIR__).'/config/'.$config_file.'.php')) {
+          $configs = require dirname(__DIR__)."/config/$config_file.php";
+          $tmp = $configs;
+          foreach ($keys as $v) {
+               if (array_key_exists($v,$tmp)) {                    
+                    $tmp = $tmp[$v];
+               }
+          }
+          return $tmp;
+     }
      return $default;
 }
 
-
-config('database.drivers.mysql.host');
+/**
+ * Normalize directory path according to what system
+ *
+ * @version 1
+ * @subpackage 1
+ *
+ * @param string $path
+ * @return string
+ **/
+function normalizeDirectoryPath($path){
+     $path =  str_replace('//',DIRECTORY_SEPARATOR,$path);
+     return $path;
+}
